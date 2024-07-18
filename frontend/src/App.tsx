2bb4527539895623f884b1e98f7dc191dde0987a
arinjain111 <arinjain111@gmail.com>
2024-07-18 17:29:33 +0530

    updated foonts in css and added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import { Signup } from './pages/Signup'
 import { Signin} from './pages/Signin'
 import { Post } from './pages/Post'
 import { Posts } from './pages/Posts'
+import { Home } from './pages/Home'
+import { Write } from './HomepageComponent/Write'
+import { OurStory } from './HomepageComponent/OurStory'
 
 function App() {
 
@@ -11,10 +14,13 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/' element= {<Home />} ></Route>
         <Route path='/Signup' element= {<Signup />}></Route>
         <Route path='/Signin' element= {<Signin />}></Route>
         <Route path='/Post/:id' element= {<Post />}></Route>
         <Route path='/Posts' element= {<Posts />}></Route>
+        <Route path='/Write' element={<Write />}></Route>
+        <Route path='/OurStory' element={<OurStory />}></Route>
       </Routes>
     </BrowserRouter>
     </>
